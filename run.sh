4fb576ebd0544f0edecadbff2bb6fd2b3d70d2ed
ashug1107 <ashleshagosavi2023.comp@mmcoe.edu.in>
2026-02-13 18:09:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 # Start FastAPI (Backend) in background
 uvicorn main:app --host 0.0.0.0 --port 8000 &
 
+sleep 20
+
 # Start Streamlit (Frontend)
 streamlit run app.py --server.port 10000 --server.address 0.0.0.0
