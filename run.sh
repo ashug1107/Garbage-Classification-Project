4b2c571d55ad05f8f60a4287591737e7e716ee51
ashug1107 <ashleshagosavi2023.comp@mmcoe.edu.in>
2026-02-14 22:29:18 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Run Streamlit on the PUBLIC port Render provides
-streamlit run app.py --server.port 10000 --server.address 0.0.0.0
+streamlit run app.py --server.port $PORT --server.address 0.0.0.0
