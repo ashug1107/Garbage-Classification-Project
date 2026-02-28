9f3f339fe3a01f6ca173b5114724bf8612618484
ashug1107 <ashleshagosavi2023.comp@mmcoe.edu.in>
2026-03-01 00:36:48 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-# Start backend on 8000 (internal)
-uvicorn main:app --host 127.0.0.1 --port 8000 &
-# Start frontend on Render's required $PORT (external)
-streamlit run app.py --server.port $PORT --server.address 0.0.0.0
